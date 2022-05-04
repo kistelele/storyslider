54eeb0e9d88cec8f08d5192c4d157b040103c30c
Nico Kistela <104937898+kistelele@users.noreply.github.com>
2022-05-04 22:15:31 +0200

    Add files via upload

---
+++ slider.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Improved Instagram Stroy like slider</title>
+<link rel="stylesheet" href="/src/css/style-slide.css">
+<link rel="stylesheet" href="/src/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+</head>
+<body>
+  <div class="back">
+      <a href="https://www.nicokistela.de">
+      <i class="fa-solid fa-x"></i>
+    </a>
+  </div>
+    <div class="mobile-slide center-screen">
+      <div data-slide="slide" class="slide">
+        <div class="slide-items">
+          <img src="https://i.picsum.photos/id/704/1000/2000.jpg?hmac=HwMyEyW_T3COO2egWxl4Wp70_rB9D7pe-gA1apG1J7A" alt="Img 1">
+          <img src="https://i.picsum.photos/id/815/1000/2000.jpg?hmac=SOizRxB5DPpSGvf2uSMRn4HiExuIQU96BlIJ7d_ZQAQ" alt="Img 2">
+          <img src="https://i.picsum.photos/id/135/1000/2000.jpg?hmac=g75DK7mH4OIzBI0lJ2bN-qcQpqJHRV6TbmCDBcbGGgM" alt="Img 3">
+          <img src="https://i.picsum.photos/id/214/1000/2000.jpg?hmac=useE5hFxjifR3v9xXpi5MGDkg_4DYDotc9-x3eic1Sk" alt="Img 4">
+          <img src="https://i.picsum.photos/id/259/1000/2000.jpg?hmac=w5xLhQopVaBbglUkWyfDvP-p-5Mhwawul8iO36ImYSk" alt="Img 5">
+        </div>
+        <nav class="slide-nav">
+          <div class="slide-thumb"></div>
+          <button class="slide-prev"></button>
+          <button class="slide-next"></button>
+        </nav>
+      </div>
+    </div>
+<script type="text/javascript" src=/src/js/slide-stories.js></script>
+<script type="text/javascript" src=/src/js/bootstrap.bundle.js></script>
+</body>
+</html>
